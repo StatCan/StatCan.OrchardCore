80bcfb33b65e1dae7579160c72019d882377b277
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2020-06-15 10:04:31 -0400

    feat(admin): copy TheAdmin theme assets
    Custom theme based on the OC admin theme
    Added language toggle on the top right corner

---
+++ src/Themes/DigitalAdmin/Manifest.cs
@@ -0,0 +1,12 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "Digtal Admin Theme",
+    Author = "Digital Innovation Team",
+    Website = "https://innovation.statcan.gc.ca",
+    Version = "1.0.0",
+    Description = "A custom admin theme.",
+    Tags = new [] { ManifestConstants.AdminTag },
+    BaseTheme = "TheAdmin"
+)]
