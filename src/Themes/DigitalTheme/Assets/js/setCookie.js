2b128f3985d489977c7cb3a8f507d5007b0d9416
M-A Boucher <marc-antoine.boucher@canada.ca>
2020-07-23 13:10:55 -0400

    feat: wip

---
+++ src/Themes/DigitalTheme/Assets/js/setCookie.js
@@ -0,0 +1,2 @@
+// Sets the cookie based on the current html lang. This is to avoid potential issues when landing on a french page.
+document.cookie = '.AspNetCore.Culture=c%3D'+document.documentElement.lang+'%7Cuic%3D'+document.documentElement.lang +';path=/';
