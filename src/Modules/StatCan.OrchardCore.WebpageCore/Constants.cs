222af603f2162b8f42ca31c3aafba89c2e2097b6
M-A-Boucher <61983856+M-A-Boucher@users.noreply.github.com>
2020-08-24 08:21:47 -0400

    Digital Theme (#57)
    feat: Add new Digital theme and WebpageCore module
    
    The WebpageCore module will be the foundation module for existing and future projects. It will contain common basic webpage components.
    
    Co-authored-by: Brendan Bronzan <brendan.bronzan@canada.ca>
    Co-authored-by: Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
    Co-authored-by: Michael Fan <michael.fan@canada.ca>

---
+++ src/Modules/StatCan.OrchardCore.WebpageCore/Constants.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatCan.OrchardCore.WebpageCore
+{
+    public static class Constants
+    {
+        public static class Features
+        {
+            public const string WebpageCore = "StatCan.OrchardCore.WebpageCore";
+            public const string PageLayout = "StatCan.OrchardCore.WebpageCore.PageLayout";
+            public const string ContentLayout = "StatCan.OrchardCore.WebpageCore.ContentLayout";
+            public const string MenuItemParts = "StatCan.OrchardCore.WebpageCore.MenuItemParts";
+        }
+    }
+}
---
+++ src/Themes/DigitalTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "DigitalTheme",
+    Author = "Digital Innovation Team",
+    Website = "https://innovation.statcan.gc.ca",
+    Version = "1.0.0",
+    Description = "Theme of Innovation Team website"
+)]
