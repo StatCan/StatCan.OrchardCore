05535af05be0a5486410b0341b1a26ccd1b42c0c
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2020-09-16 06:13:15 -0400

    admintheme

--- src/Modules/StatCan.OrchardCore.GitHub/Manifest.cs
+++ src/Modules/StatCan.OrchardCore.GitHub/Manifest.cs
@@ -4,7 +4,7 @@ using StatCan.OrchardCore.GitHub;
 [assembly: Module(
     Name = GitHubConstants.Features.GitHub,
     Author = "Digital Innovation Team",
-    Website = "https://innovation.statcan.gc.ca",
+    Website = "https://digital.statcan.gc.ca",
     Version = "1.0.0",
     Description = "Workflow tasks to interact with the github api.",
     Category = "GitHub",
