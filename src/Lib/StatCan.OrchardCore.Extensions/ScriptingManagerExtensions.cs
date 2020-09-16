ebb7cc4607ba36d96041d90a989a439ed5779bf3
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2020-09-16 09:16:45 -0400

    feat: add Extension librairy
    Library meant to house some extensions

---
+++ src/Lib/StatCan.OrchardCore.Extensions/ScriptingManagerExtensions.cs
@@ -0,0 +1,19 @@
+using OrchardCore.Scripting;
+
+namespace StatCan.OrchardCore.Extensions
+{
+    public static class ScriptingManagerExtensions
+    {
+        /// <summary>
+        /// Executes javascript by prefixing the script .
+        /// </summary>
+        /// <param name="script">The script to evaluate</param>
+        /// <param name="scopedMethodProviders">A list of method providers scoped to the script evaluation.</param>
+        /// <returns>The result of the script if any.</returns>
+        public static object EvaluateJs( this IScriptingManager  scriptingManager, string script, params IGlobalMethodProvider[] scopedMethodProviders)
+        {
+            var directive = $"js:{script}";
+            return scriptingManager.Evaluate(directive, null, null, scopedMethodProviders);
+        }
+    }
+}
