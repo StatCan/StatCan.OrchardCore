5b76d5d3403b1419543faf5eeab157eca964483a
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2020-09-17 07:05:14 -0400

    fix: add local resources

--- src/Modules/StatCan.OrchardCore.VueForms/Assets/vue-forms.js
+++ src/Modules/StatCan.OrchardCore.VueForms/Assets/vue-forms.js
@@ -5,8 +5,38 @@ Vue.component('validation-provider', VeeValidate.ValidationProvider);
 Vue.component('validation-observer', VeeValidate.ValidationObserver); 
 
 // include default french translations. The english translations are already included in the bundle
-VeeValidate.localize('fr',
-  {
+VeeValidate.localize({
+  en:{
+    "code": "en",
+    "messages": {
+      "alpha": "The {_field_} field may only contain alphabetic characters",
+      "alpha_num": "The {_field_} field may only contain alpha-numeric characters",
+      "alpha_dash": "The {_field_} field may contain alpha-numeric characters as well as dashes and underscores",
+      "alpha_spaces": "The {_field_} field may only contain alphabetic characters as well as spaces",
+      "between": "The {_field_} field must be between {min} and {max}",
+      "confirmed": "The {_field_} field confirmation does not match",
+      "digits": "The {_field_} field must be numeric and exactly contain {length} digits",
+      "dimensions": "The {_field_} field must be {width} pixels by {height} pixels",
+      "email": "The {_field_} field must be a valid email",
+      "excluded": "The {_field_} field is not a valid value",
+      "ext": "The {_field_} field is not a valid file",
+      "image": "The {_field_} field must be an image",
+      "integer": "The {_field_} field must be an integer",
+      "length": "The {_field_} field must be {length} long",
+      "max_value": "The {_field_} field must be {max} or less",
+      "max": "The {_field_} field may not be greater than {length} characters",
+      "mimes": "The {_field_} field must have a valid file type",
+      "min_value": "The {_field_} field must be {min} or more",
+      "min": "The {_field_} field must be at least {length} characters",
+      "numeric": "The {_field_} field may only contain numeric characters",
+      "oneOf": "The {_field_} field is not a valid value",
+      "regex": "The {_field_} field format is invalid",
+      "required_if": "The {_field_} field is required",
+      "required": "The {_field_} field is required",
+      "size": "The {_field_} field size must be less than {size}KB"
+    }
+  },
+  fr: {
     "code": "fr",
     "messages": {
       "alpha": "Le champ {_field_} ne peut contenir que des lettres",
@@ -35,7 +65,8 @@ VeeValidate.localize('fr',
       "required_if": "Le champ {_field_} est obligatoire lorsque {target} possède cette valeur",
       "size": "Le champ {_field_} doit avoir un poids inférieur à {size}KB"
     }
-  });
+  },
+});
 
 // run init script
 function initForm(app) {
---
+++ src/Modules/StatCan.OrchardCore.VueForms/ResourceManifest.cs
@@ -0,0 +1,49 @@
+using OrchardCore.ResourceManagement;
+
+namespace StatCan.Themes.BootstrapTheme
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(IResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest
+                .DefineScript("vuejs")
+                .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vue.min.js", "~/StatCan.OrchardCore.VueForms/Scripts/vue.js")
+                .SetCdn("https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js", "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.js")
+                .SetCdnIntegrity("sha384-OZmxTjkv7EQo5XDMPAmIkkvywVeXw59YyYh6zq8UKfkbor13jS+5p8qMTBSA1q+F", "sha384-+jvb+jCJ37FkNjPyYLI3KJzQeD8pPFXUra3B/QJFqQ3txYrUPIP1eOfxK4h3cKZP")
+                .SetVersion("2.6.11");
+            manifest
+                .DefineScript("vee-validate")
+                .SetDependencies("vuejs")
+                .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vee-validate.full.min.js", "~/StatCan.OrchardCore.VueForms/Scripts/vee-validate.full.js")
+                .SetCdn("https://cdn.jsdelivr.net/npm/vee-validate@3.3.11/dist/vee-validate.full.min.js", "https://cdn.jsdelivr.net/npm/vee-validate@3.3.11/dist/vee-validate.full.js")
+                .SetCdnIntegrity("sha384-9WkogwiBkp0ktjKxFH+9QKX2cfiWI6UvcxOD5MExVaCnK8mqJkxubbsaeFlebNYS", "sha384-pit7Y0lTrHtTYEQDF9w3oV0fIjfPYV1r2Z5UCvNSaratRyTzwq4e+IfZXLjxM80S")
+                .SetVersion("3.3.11");
+            manifest
+                .DefineScript("vuetify")
+                .SetDependencies("vuejs")
+                .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vuetify.min.js", "~/StatCan.OrchardCore.VueForms/Scripts/vuetify.js")
+                .SetCdn("https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.min.js", "https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.js")
+                .SetCdnIntegrity("sha384-HPkhWtgo1Yqb2jzqZJ91xDQ+c+Rlj6ANjQN5srMDB5RUsDI2Ff/WKBrvEbv3id+c", "sha384-fonCq3wQKhz2JW3eDMQRRpuRAFhwaqa6dt6XrsCIXBtBo3bMbAdDLtrAOxeVFMId")
+                .SetVersion("2.3.10");
+            manifest
+                .DefineStyle("vuetify")
+                .SetUrl("~/StatCan.OrchardCore.VueForms/Styles/vuetify.min.css", "~/StatCan.OrchardCore.VueForms/Styles/vuetify.css")
+                .SetCdn("https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.min.css", "https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.css")
+                .SetCdnIntegrity("sha384-lZ/7sTmigDtUbdDwbW5+uR0ev6DiONA348CAcJbzaKaKSife4dMk1+77FDwBjd52", "sha384-UH/a0cVop3B4MebOcqXSKp3HknBnXCo1m7wRkQCyqNw0s9xUORnEdydOCf6hlZhS")
+                .SetVersion("2.3.10");
+
+            manifest
+                .DefineStyle("vue-forms")
+                .SetUrl("~/StatCan.OrchardCore.VueForms/Styles/vue-forms.min.css", "~/StatCan.OrchardCore.VueForms/Styles/vue-forms.css")
+                .SetVersion("1.0.0");
+            manifest
+               .DefineScript("vue-forms")
+               .SetDependencies("vuejs", "vuetify", "vee-validate")
+               .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vue-forms.js", "~/StatCan.OrchardCore.VueForms/Scripts/vue-forms.js")
+               .SetVersion("1.0.0");
+        }
+    }
+}
