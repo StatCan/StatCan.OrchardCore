8c6c95a5d4e69f729d14328b92fdefaa29dafdcf
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2020-10-07 14:43:45 -0400

    fix: missing dependency on WebpageCore module
    Renamed some recipes, migrations.
    Added some missing feature to the Digital setup recipe

---
+++ test/cypress/integration/portal-theme.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+import { generateTenantInfo } from '../support/objects';
+
+describe("Portal theme tests", function() {    
+  let tenant;
+
+  before(() => {
+    // generate a tenant for all tests below
+    tenant = generateTenantInfo("portal-theme-setup")
+    cy.newTenant(tenant);
+    cy.login(tenant);
+  })
+  it("Recipe is successfull", function() {
+    cy.login(tenant);
+  })
+});
