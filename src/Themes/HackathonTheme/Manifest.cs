8b648f668ca26aa26182b83832a78a4bb9bb4056
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-10-22 09:53:18 -0400

    remove hackathon theme

--- src/Themes/HackathonTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "HackathonTheme",
-    Author = "Digital Innovation Team",
-    Website = "https://innovation.statcan.gc.ca",
-    Version = "1.0.0",
-    Description = "Theme of the Hackathon module"
-)]
