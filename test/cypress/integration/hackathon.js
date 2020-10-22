eddc03ac927cd41c380834983de1fb1ba0ae5e2e
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-10-22 09:39:38 -0400

    fix: formatting

--- test/cypress/integration/hackathon.js
+++ test/cypress/integration/hackathon.js
@@ -5,11 +5,10 @@ describe("Hackathon Tests", function() {
   let tenant;
 
   before(() => {
-    // generate a tenant for all tests below
     tenant = generateTenantInfo("hackathon-setup-recipe")
     cy.newTenant(tenant);
-    cy.login(tenant);
   })
+  
   it("Can login to Hackathon site", function() {
     cy.login(tenant);
   })
