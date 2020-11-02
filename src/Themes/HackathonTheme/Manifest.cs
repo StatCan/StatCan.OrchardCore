e3bbf4d39cc62b7169bd9728ff2e86cd819fd27d
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-02 16:27:04 -0500

    chore: remove Theme (moved in master)

--- src/Themes/HackathonTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "HackathonTheme",
-    Author = "Digital Innovation Team",
-    Website = "https://digital.statcan.gc.ca",
-    Version = "1.0.",
-    Description = "Hackathon platform theme"
-)]
--- src/Themes/HackathonTheme/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  outputDir: "wwwroot/dist"
-}
