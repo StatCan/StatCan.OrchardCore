6cd0071e79a72820f95cdd965cb714c349913f9e
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-09 14:01:17 -0500

    fix: re-add some tests

--- test/cypress/integration/vue-form.js
+++ test/cypress/integration/vue-form.js
@@ -103,5 +103,9 @@ describe("VueForm Tests", function() {
     cy.getByCy('form-field-name').closest('.v-input').find('.v-messages__message').should('contain', 'The name is required');
     cy.getByCy('form-errormessage').contains('An error occurred while submitting your form.');
 
+    cy.getByCy('form-button-formClear').click();	
+    cy.getByCy('form-field-gender').should('be.empty');	
+    cy.getByCy('form-field-email').should('be.empty');
+
   })
 });
