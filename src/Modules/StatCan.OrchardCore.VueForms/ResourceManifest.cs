be03ed5c6f0e02f498ed097b14fdbcb342c70a8b
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-10 09:10:05 -0500

    chore: update npm dependencies

--- src/Modules/StatCan.OrchardCore.VueForms/ResourceManifest.cs
+++ src/Modules/StatCan.OrchardCore.VueForms/ResourceManifest.cs
@@ -11,32 +11,32 @@ namespace StatCan.OrchardCore.VueForms
             manifest
                 .DefineScript("vuejs")
                 .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vue.min.js", "~/StatCan.OrchardCore.VueForms/Scripts/vue.js")
-                .SetCdn("https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js", "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.js")
-                .SetCdnIntegrity("sha384-OZmxTjkv7EQo5XDMPAmIkkvywVeXw59YyYh6zq8UKfkbor13jS+5p8qMTBSA1q+F", "sha384-+jvb+jCJ37FkNjPyYLI3KJzQeD8pPFXUra3B/QJFqQ3txYrUPIP1eOfxK4h3cKZP")
-                .SetVersion("2.6.11");
+                .SetCdn("https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js", "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js")
+                .SetCdnIntegrity("sha384-cwVe6U8Tq7F/3JIj6xeDzOwuqeChcmRcdYqDGfoYmdAurw7L3f4dFHhEJKfxv96A", "sha384-ma9ivURrHX5VOB4tNq+UiGNkJoANH4EAJmhxd1mmDq0gKOv88wkKZOfRDOpXynwh")
+                .SetVersion("2.6.12");
 
             manifest
                 .DefineScript("vee-validate")
                 .SetDependencies("vuejs")
                 .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vee-validate.full.min.js", "~/StatCan.OrchardCore.VueForms/Scripts/vee-validate.full.js")
-                .SetCdn("https://cdn.jsdelivr.net/npm/vee-validate@3.3.11/dist/vee-validate.full.min.js", "https://cdn.jsdelivr.net/npm/vee-validate@3.3.11/dist/vee-validate.full.js")
-                .SetCdnIntegrity("sha384-9WkogwiBkp0ktjKxFH+9QKX2cfiWI6UvcxOD5MExVaCnK8mqJkxubbsaeFlebNYS", "sha384-pit7Y0lTrHtTYEQDF9w3oV0fIjfPYV1r2Z5UCvNSaratRyTzwq4e+IfZXLjxM80S")
-                .SetVersion("3.3.11");
+                .SetCdn("https://cdn.jsdelivr.net/npm/vee-validate@3.4.4/dist/vee-validate.full.min.js", "https://cdn.jsdelivr.net/npm/vee-validate@3.4.4/dist/vee-validate.full.js")
+                .SetCdnIntegrity("sha384-tn0idebO6GZEY8mGV35ylsBfl51vwSVL055B3Y20gE0Q6p+UqX99nUWu9k4pVAhv", "sha384-zxq+aE29vXErUV36F/msIf+ZrGzTn0wKRxtAyz/vT0Ga2XSM86KfyK+qGJNnjXXZ")
+                .SetVersion("3.4.4");
 
             manifest
                 .DefineScript("vuetify")
                 .SetDependencies("vuejs")
                 .SetUrl("~/StatCan.OrchardCore.VueForms/Scripts/vuetify.min.js", "~/StatCan.OrchardCore.VueForms/Scripts/vuetify.js")
-                .SetCdn("https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.min.js", "https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.js")
-                .SetCdnIntegrity("sha384-HPkhWtgo1Yqb2jzqZJ91xDQ+c+Rlj6ANjQN5srMDB5RUsDI2Ff/WKBrvEbv3id+c", "sha384-fonCq3wQKhz2JW3eDMQRRpuRAFhwaqa6dt6XrsCIXBtBo3bMbAdDLtrAOxeVFMId")
-                .SetVersion("2.3.10");
+                .SetCdn("https://cdn.jsdelivr.net/npm/vuetify@2.3.16/dist/vuetify.min.js", "https://cdn.jsdelivr.net/npm/vuetify@2.3.16/dist/vuetify.js")
+                .SetCdnIntegrity("sha384-oWs61kcvUHUcvKDasUC1/NOLdo+TgLyQ6naS89TiEz+0/rM1YvEEwSQWQdfvgi8X", "sha384-KHkNwxwHw/d8M1bpUtFKyKgIbSGPUiZZRkN2MIoY/YTkFulpgXUGDD58NfaaMZm/")
+                .SetVersion("2.3.16");
 
             manifest
                 .DefineStyle("vuetify")
                 .SetUrl("~/StatCan.OrchardCore.VueForms/Styles/vuetify.min.css", "~/StatCan.OrchardCore.VueForms/Styles/vuetify.css")
-                .SetCdn("https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.min.css", "https://cdn.jsdelivr.net/npm/vuetify@2.3.10/dist/vuetify.css")
-                .SetCdnIntegrity("sha384-lZ/7sTmigDtUbdDwbW5+uR0ev6DiONA348CAcJbzaKaKSife4dMk1+77FDwBjd52", "sha384-UH/a0cVop3B4MebOcqXSKp3HknBnXCo1m7wRkQCyqNw0s9xUORnEdydOCf6hlZhS")
-                .SetVersion("2.3.10");
+                .SetCdn("https://cdn.jsdelivr.net/npm/vuetify@2.3.16/dist/vuetify.min.css", "https://cdn.jsdelivr.net/npm/vuetify@2.3.16/dist/vuetify.css")
+                .SetCdnIntegrity("sha384-C0yQzReUZVYGwxgpET43LgD1tn0L3Hv2/HSL6RTcL1HLPCPHBf7QHY+Y+TPBGZxp", "sha384-aBPEGa60gC4/krAhSCZzRWSdXY4rLG2KWrugfUKftJNVdXcncArtH4LFesz5LaxF")
+                .SetVersion("2.3.16");
 
             manifest
                 .DefineStyle("vue-forms")
