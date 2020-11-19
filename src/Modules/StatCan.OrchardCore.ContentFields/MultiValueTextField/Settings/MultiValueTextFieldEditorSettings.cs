f192a09320acc54e7303cde557f3285510f804b2
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-19 10:34:55 -0500

    rename some options

--- src/Modules/StatCan.OrchardCore.ContentFields/MultiValueTextField/Settings/MultiValueTextFieldEditorSettings.cs
+++ src/Modules/StatCan.OrchardCore.ContentFields/MultiValueTextField/Settings/MultiValueTextFieldEditorSettings.cs
@@ -6,11 +6,11 @@ namespace StatCan.OrchardCore.ContentFields.MultiValueTextField.Settings
     public class MultiValueTextFieldEditorSettings
     {
         public ListValueOption[] Options { get; set; }
-        public EditorOption Editor { get; set; }
+        public MultiValueEditorOption Editor { get; set; }
         public string DefaultValue { get; set; }
     }
 
-    public enum EditorOption
+    public enum MultiValueEditorOption
     {
         Checkbox,
         Dropdown,
--- src/Modules/StatCan.OrchardCore.ContentFields/PredefinedGroup/Settings/TextFieldPredefinedGroupEditorSettings.cs
+++ src/Modules/StatCan.OrchardCore.ContentFields/PredefinedGroup/Settings/TextFieldPredefinedGroupEditorSettings.cs
@@ -6,11 +6,11 @@ namespace StatCan.OrchardCore.ContentFields.PredefinedGroup.Settings
     public class TextFieldPredefinedGroupEditorSettings
     {
         public ListValueOption[] Options { get; set; }
-        public EditorOption Editor { get; set; }
+        public PredefinedGroupEditorOptions Editor { get; set; }
         public string DefaultValue { get; set; }
     }
 
-    public enum EditorOption
+    public enum PredefinedGroupEditorOptions
     {
         ButtonGroup
     }
