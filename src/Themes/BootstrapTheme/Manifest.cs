2fa96d5418781941d6d5144a6ba571391890fcb4
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-26 14:16:24 -0500

    refactor: use constants in the Manifest.cs files
    Also cleaned up non needed references in the csproj

--- src/Themes/BootstrapTheme/Manifest.cs
+++ src/Themes/BootstrapTheme/Manifest.cs
@@ -1,9 +1,10 @@
 using OrchardCore.DisplayManagement.Manifest;
+using static StatCan.OrchardCore.Manifest.StatCanManifestConstants;
 
 [assembly: Theme(
     Name = "BootstrapTheme",
-    Author = "Digital Innovation Team",
-    Website = "https://digital.statcan.gc.ca",
-    Version = "1.0.0",
+    Author = DigitalInnovationTeam,
+    Website = DigitalInnovationWebsite,
+    Version = Version,
     Description = "Bootstrap based theme. Meant to be used as a good starting point to build digital websites"
 )]
--- src/Themes/DigitalAdmin/Manifest.cs
+++ src/Themes/DigitalAdmin/Manifest.cs
@@ -1,11 +1,11 @@
 using OrchardCore.DisplayManagement.Manifest;
-using OrchardCore.Modules.Manifest;
+using static StatCan.OrchardCore.Manifest.StatCanManifestConstants;
 
 [assembly: Theme(
     Name = "Digtal Admin Theme",
-    Author = "Digital Innovation Team",
-    Website = "https://digital.statcan.gc.ca",
-    Version = "1.0.0",
+    Author = DigitalInnovationTeam,
+    Website = DigitalInnovationWebsite,
+    Version = Version,
     Description = "A custom admin theme.",
     Tags = new [] { "Admin" },
     BaseTheme = "TheAdmin"
