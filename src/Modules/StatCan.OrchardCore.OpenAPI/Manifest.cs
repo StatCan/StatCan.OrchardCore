1b1fc02dd9962442dda712e53f05519f78b1c142
Michael Fan <michael.fan@canada.ca>
2020-12-11 15:47:35 -0500

    feat: initial commit for OpenAPI

---
+++ src/Modules/StatCan.OrchardCore.OpenAPI/Manifest.cs
@@ -0,0 +1,20 @@
+using OrchardCore.Modules.Manifest;
+using static StatCan.OrchardCore.OpenAPI.FeatureIds;
+
+[assembly: Module(
+    Name = "StatCan OpenAPI",
+    Author = "Digital Innovation Team",
+    Website = "https://digital.statcan.gc.ca",
+    Version = "1.0.0"
+)]
+
+[assembly: Feature(
+    Id = OpenAPI,
+    Name = "StatCan.OpenAPI - Widgets",
+    Category = "Content",
+    Description = "Adds a widget that displays the Swagger UI",
+    Dependencies = new[]
+    {
+        "OrchardCore.Widgets"
+    }
+)]
