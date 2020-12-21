0ef30edd28be68cdfde1c8ea742aa049ba7a9569
Rubylynne Axalan <rubylynne.axalan@gmail.com>
2020-12-21 08:19:29 -0500

    Adds Assessment recipe

--- test/cypress/integration/assessment.js
+++ test/cypress/integration/assessment.js
@@ -3,9 +3,14 @@ import { generateTenantInfo } from 'cypress-orchardcore/dist/utils';
 
 describe("Assessment Test", function() {    
   it("Create Assessment tenant ", function() {
-    let tenant = generateTenantInfo("digital-theme-setup", "Assessment module tests")
+    let tenant = generateTenantInfo("bootstrap-theme-setup", "Assessment module tests")
     cy.newTenant(tenant);
     cy.login(tenant);
     cy.enableFeature(tenant, "StatCan_OrchardCore_Assessment");
   })
+
+  //Create Assessment Content Type
+
+
+
 });
