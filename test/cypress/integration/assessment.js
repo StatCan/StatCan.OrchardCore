6c438e087b8ae70e419afb97a9af69efadcd587a
axalrub <rubylynne.axalan@gmail.com>
2021-01-13 16:05:17 -0500

    Cleans code as per JP's review

--- test/cypress/integration/assessment.js
+++ test/cypress/integration/assessment.js
@@ -1,6 +1,13 @@
 /// <reference types="Cypress" />
 import { generateTenantInfo } from 'cypress-orchardcore/dist/utils';
 
+
+before(function () {
+  cy.fixture('assessment.json').then(function (dataJSON) {
+    this.data = dataJSON;
+  })
+})
+
 describe("Assessment Test", function() {    
   let tenant;
   it("Create Assessment tenant ", function() {
@@ -19,10 +26,14 @@ describe("Assessment Test", function() {
 
   //Add Assessment widget
   it("Can create an assessment form", function() {
+    cy.fixture('assessment.json').as('dataJSON')
     cy.visit(`${tenant.prefix}/login`)
     cy.login(tenant);
     cy.visit(`${tenant.prefix}/Admin/Contents/ContentTypes/Assessment/create`);
-    cy.get("input[name='Assessment.Data.Text']").type('{{}"completedHtml": "Thank you for completing the assessment","title": "Confidentiality Classification Tool","description": "Methodology: The risk assessment of a selected product is performed by using a checklist to associate one of three ratings, LOW, MEDIUM or HIGH, with how much effort (SIGNIFICANT, SOME or LITTLE) that a third party must expend in order to disclose confidential information once the product has become available.","pages": [{{}"name": "page1","elements": [{{}"type": "radiogroup","name": "question1","title": "Can confidential information be revealed directly through the information displayed by the product? ","isRequired": true,"choices": [{{}"value": "item1-1","text": "LOW risk as SIGNIFICANT effort is required","score": 1},{{}"value": "item2-2","text": "MEDIUM risk as SOME effort is required","score": 2},{{}"value": "item3-3","text": "HIGH risk as LITTLE effort is required","score": 3}]},{{}"type": "radiogroup","name": "question4","title": "Can confidential information be revealed by grouping attributes available in the product?","isRequired": true,"choices": [{{}"value": "item1","text": "LOW risk as SIGNIFICANT effort is required","score": 1},{{}"value": "item2","text": "MEDIUM risk as SOME effort is required","score": 2},{{}"value": "item3","text": "HIGH risk as LITTLE effort is required","score": 3}]},{{}"type": "radiogroup","name": "question3","title": "Can confidential information be revealed through a probabilistic statement derived from the information contained in the product?","isRequired": true,"choices": [{{}"value": "item1","text": "LOW risk as SIGNIFICANT effort is required","score": 1},{{}"value": "item2","text": "MEDIUM risk as SOME effort is required","score": 2},{{}"value": "item3","text": "HIGH risk as LITTLE effort is required","score": 3}]},{{}"type": "radiogroup","name": "question2","title": "Can confidential information be revealed by combining the information contained in this product with that from other products?","isRequired": true,"choices": [{{}"value": "item1","text": "LOW risk as SIGNIFICANT effort is required","score": 1},{{}"value": "item2","text": "MEDIUM risk as SOME effort is required","score": 2},{{}"value": "item3","text": "HIGH risk as LITTLE effort is required","score": 3}]}],"title": "Disclosure Risk Self-Assessment Checklist","description": "PURPOSE: Provide a self-assessment of the disclosure risk associated with the product under examination."},{{}"name": "page2","elements": [{{}"type": "radiogroup","name": "question5","title": "Select the highest level of impact a breach would have on an individual, business, or institution from the Sensitivity Rating Legend below, and provide a justification for why you believe this is the case.  The impact must reflect an outcome that can reasonably be expected to occur, rather than one based on an extreme scenario.","isRequired": true,"choices": [{{}"value": "item1","text": "Severe (5)","score": 5},{{}"value": "item2","text": "High (4)","score": 4},{{}"value": "item3","text": "Medium (3)","score": 3},{{}"value": "item4","text": "Low (2)","score": 2}],"hasOther": true,"otherText": "Negligible (1)","score": 1}],"title": "Sensitivity Self-Assessment Checklist","description": "To assess the level of sensitivity of the specified product based on the impact a breach would have on an individual, business or institution."}]}', {force:true});
+    cy.fixture('assessment.json').then((dataJSON) => {
+      cy.get("input[name='Assessment.Data.Text']")
+        .type(JSON.stringify(dataJSON),{ force: true, parseSpecialCharSequences: false })
+    })
     cy.get('.btn-success').click();
   })
 
@@ -31,6 +42,6 @@ describe("Assessment Test", function() {
     cy.visit(`${tenant.prefix}/login`)
     cy.login(tenant);
     cy.visit(`${tenant.prefix}/Admin/Contents/ContentItems`);
-    cy.get('.float-right').click();
+    cy.get('.float-right > .btn-success').click();
   })
-});
+})
