d76b76c644b6ccad26a53a1021d0ff239955785c
axalrub <rubylynne.axalan@gmail.com>
2021-02-02 10:34:30 -0500

    removes recipe, adds Migration instructions

--- src/Modules/StatCan.OrchardCore.Tabulator/Migrations.cs
+++ src/Modules/StatCan.OrchardCore.Tabulator/Migrations.cs
@@ -1,3 +1,4 @@
+using OrchardCore.ContentFields.Settings;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
@@ -16,7 +17,65 @@ namespace StatCan.OrchardCore.Tabulator
 
         public int Create()
         {
+            TableCreator();
             return 1;
         }
+
+        private void TableCreator() {
+            _contentDefinitionManager.AlterTypeDefinition("TableCreator", type => type
+                .DisplayedAs("TableCreator")
+                .Stereotype("Widget")
+                .WithPart("TableCreator", part => part
+                    .WithPosition("0")
+                )
+            );
+
+            _contentDefinitionManager.AlterPartDefinition("TableCreator", part => part
+                .WithField("TableData", field => field
+                    .OfType("TextField")
+                    .WithDisplayName("TableData")
+                    .WithPosition("0")
+                    .WithSettings(new TextFieldSettings
+                    {
+                        Hint = "Enter JSON for Table Data here",
+                        Required = true,
+                    })
+                )
+                .WithField("ColumnsData", field => field
+                    .OfType("TextField")
+                    .WithDisplayName("ColumnsData")
+                    .WithPosition("1")
+                    .WithSettings(new TextFieldSettings
+                    {
+                        Hint = "To specify the column headers, enter the JSON for the columns here. If using the AutoColumnizer feature, enter an empty set of quotation marks. Eg. \"\".",
+                        Required = true,
+                    })
+                )
+                .WithField("AutoColumnizer", field => field
+                    .OfType("TextField")
+                    .WithDisplayName("AutoColumnizer")
+                    .WithPosition("2")
+                    .WithSettings(new TextFieldSettings
+                    {
+                        Hint = "Enter \"true\" to automatically set the columns as per the TableData. Enter \"false\" to set the columns as per the json in the ColumnsData field.",
+                    })
+                )
+                .WithField("PaginationSize", field => field
+                    .OfType("NumericField")
+                    .WithDisplayName("PaginationSize")
+                    .WithPosition("2")
+                    .WithSettings(new NumericFieldSettings
+                    {
+                        Hint = "Enter the number of rows to be displayed per page. For example, entering \"5\" will display 5 rows per page.",
+                        Minimum = 1,
+                        Maximum = 100,
+                    })
+                )
+            );
+            }
+
+
+
+
     }
 }
--- test/cypress/integration/tabulator.js
+++ test/cypress/integration/tabulator.js
@@ -10,39 +10,4 @@ describe("Table Creator Test", function() {
     cy.enableFeature(tenant, "StatCan_OrchardCore_Tabulator");
   })
 
-   //Run Tabulator Recipe
-   it("Can run TableCreator Recipe", function() {
-    cy.visit(`${tenant.prefix}/login`)
-    cy.login(tenant);
-    cy.runRecipe(tenant, 'Table_Creator');
-})
-
-  //Add Tabulator widget
-  it("Can create a table", function() {
-    cy.fixture('tableCreatorData.json').as('dataJSON')
-    cy.visit(`${tenant.prefix}/login`)
-    cy.login(tenant);
-    cy.visit(`${tenant.prefix}/Admin/Contents/ContentTypes/TableCreator/create`);
-    cy.fixture('tableCreatorData.json').then((dataJSON) => {
-      cy.get("input[name='TableCreator.TableData.Text']")
-        .type(JSON.stringify(dataJSON),{ force: true, parseSpecialCharSequences: false })
-    })
-    cy.fixture('tableCreatorColData.json').then((dataJSON) => {
-      cy.get("input[name='TableCreator.ColumnsData.Text']")
-      .type(JSON.stringify(dataJSON), { force: true, parseSpecialCharSequences: false })
-    })
-    cy.get("input[name='TableCreator.AutoColumnizer.Text']").type('false', {force:true});
-    cy.get("input[name='TableCreator.PaginationSize.Value']").type('5', {force:true});
-    cy.get('.btn-success').click();
-  })
-
-  //View Tabulator
-  it("Can view the Table", function() {
-    cy.visit(`${tenant.prefix}/login`)
-    cy.login(tenant);
-    cy.visit(`${tenant.prefix}/Admin/Contents/ContentItems`);
-    cy.get('.float-right > .btn-success').click();
-  })
-
-
 });
