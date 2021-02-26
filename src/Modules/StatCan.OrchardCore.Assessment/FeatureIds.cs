d40710805b5365feec4664741d8a4edf6ec308db
axalrub <rubylynne.axalan@gmail.com>
2021-02-26 14:35:27 -0500

    fixes alignment and mobile view

--- src/Modules/StatCan.OrchardCore.Assessment/FeatureIds.cs
+++
@@ -1,7 +0,0 @@
-namespace StatCan.OrchardCore.Assessment
-{
-    public static class FeatureIds
-    {
-        public const string Assessment = "StatCan.OrchardCore.Assessment";
-    }
-}
--- src/Modules/StatCan.OrchardCore.Ethos/Migrations.cs
+++ src/Modules/StatCan.OrchardCore.Ethos/Migrations.cs
@@ -1,8 +1,10 @@
+using OrchardCore.ContentFields.Settings;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
 using OrchardCore.Title.Models;
 using StatCan.OrchardCore.Extensions;
+using OrchardCore.Media.Settings;
 
 namespace StatCan.OrchardCore.Persona
 {
@@ -16,7 +18,55 @@ namespace StatCan.OrchardCore.Persona
 
         public int Create()
         {
+            CreatePersona();
             return 1;
+        }
+
+        private void CreatePersona() {
+            _contentDefinitionManager.AlterTypeDefinition("Persona", type => type
+                .DisplayedAs("Persona")
+                .Stereotype("Widget")
+                .WithPart("Persona", part => part
+                    .WithPosition("0")
+                )
+            );
+
+            _contentDefinitionManager.AlterPartDefinition("Persona", part => part
+                .WithField("Data", field => field
+                    .OfType("TextField")
+                    .WithDisplayName("Data")
+                    .WithPosition("0")
+                    .WithSettings(new TextFieldSettings
+                    {
+                        Hint = "Enter JSON for Persona here.",
+                        Required = true,
+                    })
+                )
+                .WithField("PhotoPicker", field => field
+                    .OfType("MediaField")
+                    .WithDisplayName("PhotoPicker")
+                    .WithPosition("2")
+                    .WithSettings(new MediaFieldSettings
+                    {
+                        Hint = "Select the photo to use for the persona.",
+                        Required = true,
+                        Multiple = false,
+                        AllowMediaText = false,
+                        AllowAnchors = true,
+                    })
+                )
+                .WithField("Colour", field => field
+                    .OfType("TextField")
+                    .WithDisplayName("Colour")
+                    .WithEditor("Color")
+                    .WithPosition("1")
+                    .WithSettings(new TextFieldSettings
+                    {
+                        Hint = "Choose the colour that matches the site's theme.",
+                    })
+                )
+            );
+
         }
     }
 }
