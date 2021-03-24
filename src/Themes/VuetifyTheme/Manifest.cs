5626216f520318ca795225331dfe3f9412d713bc
Marc-Antoine Boucher <marc-antoineboucher@Marc-Antoines-MacBook-Air.local>
2021-03-24 18:33:50 -0400

    feat: add vuetify theme

---
+++ src/Themes/VuetifyTheme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+using static StatCan.OrchardCore.Manifest.StatCanManifestConstants;
+
+[assembly: Theme(
+    Name = "VuetifyTheme",
+    Author = DigitalInnovationTeam,
+    Website = DigitalInnovationWebsite,
+    Version = "1.0.",
+    Description = "Vuetify platform theme"
+)]
