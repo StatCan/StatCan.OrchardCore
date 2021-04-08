6c788114c3701f346030ccf9b5a40fd2440600ef
Arnaud Roy <arnaud.roy07@gmail.com>
2021-04-08 15:29:52 -0400

    add VueDraggable component

--- src/Themes/HackathonTheme/Assets/src/App.js
+++ src/Themes/HackathonTheme/Assets/src/App.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import vuetify from './plugins/vuetify'
+import Draggable from './components/Draggable.vue'
 
 import './styles.scss'
 import './sass/github-markdown.scss'
 
 // Sets the cookie based on the current html lang. This is to avoid potential issues when landing on a french page.
 document.cookie = '.AspNetCore.Culture=c%3D'+document.documentElement.lang+'%7Cuic%3D'+document.documentElement.lang +';path=/';
+
+Vue.component('sort-draggable', Draggable);
 
 document.addEventListener("DOMContentLoaded", function() {
   new Vue({
---
+++ src/Themes/HackathonTheme/Assets/src/components/Draggable.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="row">
+    <div class="col-2">
+      <div class="form-group">
+        <div
+          class="btn-group-vertical buttons"
+          role="group"
+          aria-label="Basic example"
+        >
+        </div>
+      </div>
+    </div>
+
+    <div class="col-6">
+      <h3>Draggable</h3>
+
+      <draggable
+        :list="list"
+        class="list-group"
+        ghost-class="ghost"
+        :move="checkMove"
+        @start="dragging = true"
+        @end="dragging = false"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list"
+          :key="element.id"
+        >
+          {{ element.name }}
+        </div>
+      </draggable>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+export default {
+  name: "simple",
+  display: "Simple",
+  order: 0,
+  components: {
+    draggable
+  },
+  props: ['list'],
+  data() {
+    return {
+      dragging: false
+    };
+  },
+  computed: {
+    draggingInfo() {
+      return this.dragging ? "under drag" : "";
+    }
+  },
+  methods: {
+    checkMove: function(e) {
+      window.console.log("Future index: " + e.draggedContext.futureIndex);
+    }
+  }
+};
+</script>
+<style scoped>
+.buttons {
+  margin-top: 35px;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+</style>
