24d08051654dd62e00297062cdb45135df8fab5a
Marc-Antoine Boucher <marc-antoineboucher@Marc-Antoines-MacBook-Air.local>
2021-04-08 07:30:23 -0400

    fix: fix resource manifest

--- src/Themes/VuetifyTheme/ResourceManifest.cs
+++ src/Themes/VuetifyTheme/ResourceManifest.cs
@@ -1,28 +1,35 @@
+using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
 
 namespace StatCan.Themes.VuetifyTheme
 {
-    public class ResourceManifest : IResourceManifestProvider
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
     {
-        public void BuildManifests(IResourceManifestBuilder builder)
+        private static ResourceManifest _manifest;
+        static ResourceManagementOptionsConfiguration()
         {
-            var manifest = builder.Add();
+            _manifest = new ResourceManifest();
 
-            manifest
+            _manifest
                 .DefineStyle("vuetifytheme-styles")
                 .SetUrl("~/VuetifyTheme/dist/vuetify-theme.css", "~/VuetifyTheme/dist/vuetify-theme.css")
                 .SetVersion("1.0.0");
-            manifest
+            _manifest
                 .DefineScript("vuejs")
                 .SetUrl("~/VuetifyTheme/Scripts/vue.min.js", "~/VuetifyTheme/Scripts/vue.js")
                 .SetCdn("https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js", "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js")
                 .SetCdnIntegrity("sha384-cwVe6U8Tq7F/3JIj6xeDzOwuqeChcmRcdYqDGfoYmdAurw7L3f4dFHhEJKfxv96A", "sha384-ma9ivURrHX5VOB4tNq+UiGNkJoANH4EAJmhxd1mmDq0gKOv88wkKZOfRDOpXynwh")
                 .SetVersion("2.6.12");
-            manifest
+            _manifest
                 .DefineScript("vuetifytheme-scripts")
                 .SetDependencies("vuejs")
                 .SetUrl("~/VuetifyTheme/dist/vuetify-theme.umd.min.js", "~/VuetifyTheme/dist/vuetify-theme.umd.js")
                 .SetVersion("1.0.0");
+        }
+
+        public void Configure(ResourceManagementOptions options)
+        {
+            options.ResourceManifests.Add(_manifest);
         }
     }
 }
