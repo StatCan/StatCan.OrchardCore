7270220b3185fe8250b2d92b51fb05ac4c2f4f7c
R Axalan <rubylynne.axalan@gmail.com>
2021-04-14 14:37:36 -0400

    Uses OC shape to split canvas types

--- test/cypress/integration/canvas.js
+++ test/cypress/integration/canvas.js
@@ -4,7 +4,7 @@ before(function () {
     cy.fixture('businessCanvas.json').then(function (dataJSON) {
       this.data = dataJSON;
     })
-  })
+})
 
   describe("Canvas Test", function() {    
     let tenant;
@@ -20,42 +20,41 @@ before(function () {
       cy.visit(`${tenant.prefix}/login`)
       cy.login(tenant);
       cy.runRecipe(tenant, 'Canvas');
-  })
+    })
 
     //Create a Business Canvas
     it("Can create a Business type Canvas", function() {
-        cy.fixture('businessCanvas.json').as('dataJSON')
-        cy.visit(`${tenant.prefix}/login`)
-        cy.login(tenant);
-        cy.visit(`${tenant.prefix}/Admin/Contents/ContentTypes/Canvas/create`);
-        cy.get('[type="radio"]').check("business", {force: true})
-        cy.fixture('businessCanvas.json').then((dataJSON) => {
-          cy.get('.CodeMirror textarea')
-            .type(JSON.stringify(dataJSON),{ force: true, parseSpecialCharSequences: false })
-        })
-        cy.get('.btn-success').click();
+      cy.fixture('businessCanvas.json').as('dataJSON')
+      cy.visit(`${tenant.prefix}/login`)
+      cy.login(tenant);
+      cy.visit(`${tenant.prefix}/Admin/Contents/ContentTypes/Canvas/create`);
+      cy.get('[type="radio"]').check("business", {force: true})
+      cy.fixture('businessCanvas.json').then((dataJSON) => {
+        cy.get('.CodeMirror textarea')
+          .type(JSON.stringify(dataJSON),{ force: true, parseSpecialCharSequences: false })
       })
+      cy.get('.btn-success').click();
+    })
 
     //Create a Client Canvas
     it("Can create a Client type Canvas", function() {
-        cy.fixture('clientCanvas.json').as('dataJSON')
-        cy.visit(`${tenant.prefix}/login`)
-        cy.login(tenant);
-        cy.visit(`${tenant.prefix}/Admin/Contents/ContentTypes/Canvas/create`);
-        cy.get('[type="radio"]').check("client", {force: true})
-        cy.fixture('clientCanvas.json').then((dataJSON) => {
-        cy.get('.CodeMirror textarea')
-            .type(JSON.stringify(dataJSON),{ force: true, parseSpecialCharSequences: false })
-        })
-        cy.get('.btn-success').click();
-  })
+      cy.fixture('clientCanvas.json').as('dataJSON')
+      cy.visit(`${tenant.prefix}/login`)
+      cy.login(tenant);
+      cy.visit(`${tenant.prefix}/Admin/Contents/ContentTypes/Canvas/create`);
+      cy.get('[type="radio"]').check("client", {force: true})
+      cy.fixture('clientCanvas.json').then((dataJSON) => {
+      cy.get('.CodeMirror textarea')
+        .type(JSON.stringify(dataJSON),{ force: true, parseSpecialCharSequences: false })
+      })
+      cy.get('.btn-success').click();
+    })
 
     //View Business and Client Canvas
     it("Can view Business and Client Canvas", function() {
-        cy.visit(`${tenant.prefix}/login`)
-        cy.login(tenant);
-        cy.visit(`${tenant.prefix}/Admin/Contents/ContentItems`);
-        cy.get('.btn-success').click({multiple: true});
+      cy.visit(`${tenant.prefix}/login`)
+      cy.login(tenant);
+      cy.visit(`${tenant.prefix}/Admin/Contents/ContentItems`);
+      cy.get('.btn-success').click({multiple: true});
     })
-      
 });
