e7a544a8140a1d8a41d973b97958085b5e8b567c
Jean-Philippe Tissot <dev@tissotweb.ca>
2021-05-07 15:06:11 -0400

    update cypress deps

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -8,16 +8,3 @@ Cypress.Commands.add("loginAs", function(tenantPrefix, userName, pasword) {
   cy.get("#Password").type(pasword);
   cy.get("#UserName").closest('form').submit();
 });
-
-Cypress.Commands.overwrite("uploadRecipeJson", (originalFn, {prefix}, fixturePath) => {
-  cy.fixture(fixturePath).then((data) => {
-    cy.visit(`${prefix}/Admin/DeploymentPlan/Import/Json`);
-    cy.get('.CodeMirror').should('be.visible');
-    cy.get("body").then($body => {
-      const elem = $body.find(".CodeMirror")[0].CodeMirror.setValue(JSON.stringify(data));
-    });
-    cy.get('.ta-content > form').submit();
-    // make sure the message-success alert is displayed
-    cy.get('.message-success').should('contain', "Recipe imported");
-  });
-});
