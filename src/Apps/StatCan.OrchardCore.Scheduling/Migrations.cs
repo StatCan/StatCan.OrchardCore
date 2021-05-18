45d30b12a438ab659ee6c9b3fbddc836e3978e10
Jean-Philippe Tissot <dev@tissotweb.ca>
2021-05-18 08:59:58 -0400

    wip

--- src/Apps/StatCan.OrchardCore.Scheduling/Migrations.cs
+++ src/Apps/StatCan.OrchardCore.Scheduling/Migrations.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
 using OrchardCore.Recipes.Services;
 
@@ -7,15 +9,18 @@ namespace StatCan.OrchardCore.Scheduling
     public class Migrations : DataMigration
     {
         private readonly IRecipeMigrator _recipeMigrator;
+        private readonly IContentDefinitionManager _contentDefinitionManager;
 
-        public Migrations(IRecipeMigrator recipeMigrator)
+        public Migrations(IRecipeMigrator recipeMigrator, IContentDefinitionManager contentDefinitionManager)
         {
             _recipeMigrator = recipeMigrator;
+            _contentDefinitionManager = contentDefinitionManager;
         }
 
         public async Task<int> CreateAsync()
         {
             await _recipeMigrator.ExecuteAsync("initial.recipe.json", this);
+            _contentDefinitionManager.AlterTypeDefinition("Taxonomy", t=>t.Securable());
             return 1;
         }
     }
