0378de55371761f47a9df07205977aa543fd28ae
Jean-Philippe Tissot <dev@tissotweb.ca>
2021-05-31 15:22:11 -0400

    update startup to use new async version of host

--- src/StatCan.OrchardCore.Cms.Web/Program.cs
+++ src/StatCan.OrchardCore.Cms.Web/Program.cs
@@ -1,23 +1,24 @@
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using OrchardCore.Logging;
-using OrchardCore.Modules;
 
 namespace web
 {
     [SuppressMessage("csharp", "RCS1102", Justification = "Program file")]
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+        public static Task Main(string[] args)
+            => BuildHost(args).RunAsync();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseNLogWeb()
-                .UseStartup<Startup>()
-                .Build();
+        public static IHost BuildHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseNLogWeb()
+                    .UseStartup<Startup>()
+                ).Build();
     }
 }
