cdd7cde9e72c148304e068df4b506725a0927689
Michael Fan <62428476+mfan127@users.noreply.github.com>
2021-09-15 15:32:40 -0400

    feat: add taxonomy code migration

--- src/Apps/StatCan.OrchardCore.Radar/Startup.cs
+++ src/Apps/StatCan.OrchardCore.Radar/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 using OrchardCore.Data.Migration;
+using StatCan.OrchardCore.Radar.Migrations;
 
 namespace StatCan.OrchardCore.Radar
 {
@@ -11,7 +12,7 @@ namespace StatCan.OrchardCore.Radar
     {
         public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IDataMigration, Migrations>();
+            services.AddScoped<IDataMigration, TaxonomyMigrations>();
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
