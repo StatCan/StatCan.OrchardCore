5a19dc1116ec21e43a0a770f7fab925123d2200d
Michael Fan <62428476+mfan127@users.noreply.github.com>
2021-09-15 15:32:40 -0400

    feat: add taxonomy code migration

--- src/Apps/StatCan.OrchardCore.Radar/Constants.cs
+++ src/Apps/StatCan.OrchardCore.Radar/Constants.cs
@@ -6,5 +6,14 @@ namespace StatCan.OrchardCore.Radar
         {
             public const string Radar = "StatCan.OrchardCore.Radar";
         }
+
+        public static class ContentTypes
+        {
+            // Taxonomies
+            public const string Topic = "Topic";
+            public const string ProposalType = "ProposalType";
+            public const string ProjectType = "ProjectType";
+            public const string CommunityType = "CommunityType";
+        }
     }
 }
