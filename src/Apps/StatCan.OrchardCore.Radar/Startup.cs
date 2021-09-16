f34f8ba08554d12ddf89b46a51e956344b3a15e9
Michael Fan <62428476+mfan127@users.noreply.github.com>
2021-09-16 16:01:28 -0400

    wip: add setup recipe

--- src/Apps/StatCan.OrchardCore.Radar/Startup.cs
+++ src/Apps/StatCan.OrchardCore.Radar/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 using OrchardCore.Data.Migration;
+using StatCan.OrchardCore.Radar.Migrations;
 
 namespace StatCan.OrchardCore.Radar
 {
@@ -11,7 +12,8 @@ namespace StatCan.OrchardCore.Radar
     {
         public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IDataMigration, Migrations>();
+            services.AddScoped<IDataMigration, EntityMigration>();
+            services.AddScoped<IDataMigration, AppMigration>();
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
