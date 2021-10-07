2b4231a0e616af1680b4d9ac44a052320e6a25f1
Michael Fan <62428476+mfan127@users.noreply.github.com>
2021-10-07 14:28:27 -0400

    feat: add global search

---
+++ src/Apps/StatCan.OrchardCore.Radar/Assets/js/table.js
@@ -0,0 +1,93 @@
+const attachSorter = function() {
+  const tables = document.querySelectorAll("#entity-table");
+  for (let table of tables) {
+    const headers = table.querySelectorAll("th");
+    const tableBody = table.querySelector("tbody");
+    const rows = tableBody.querySelectorAll("tr");
+
+    // Track sort directions
+    const directions = Array.from(headers).map(function(header) {
+      return "";
+    });
+
+    // Transform the content of given cell in given column
+    const transform = function(index, content) {
+      // Get the data type of column
+      const type = headers[index].getAttribute("data-type");
+
+      if (type === "name") {
+        content = content.childNodes[0].innerHTML;
+      } else if (type === "number") {
+        content = content.childNodes[0].childNodes[2].innerHTML;
+      } else if (type === "date-range") {
+        content = content.childNodes[0].innerHTML.split("-")[0];
+      }
+
+      switch (type) {
+        case "number":
+          return parseFloat(content);
+        case "date":
+          return Date.parse(content);
+        case "string":
+        default:
+          return content;
+      }
+    };
+
+    const sortColumn = function(index) {
+      // Get the current direction
+      const direction = directions[index] || "asc";
+
+      // A factor based on the direction
+      const multiplier = direction === "asc" ? 1 : -1;
+
+      const newRows = Array.from(rows);
+
+      newRows.sort(function(rowA, rowB) {
+        const cellA = rowA.querySelectorAll("td")[index];
+        const cellB = rowB.querySelectorAll("td")[index];
+
+        const a = transform(index, cellA);
+        const b = transform(index, cellB);
+
+        switch (true) {
+          case a > b:
+            return 1 * multiplier;
+          case a < b:
+            return -1 * multiplier;
+          case a === b:
+            return 0;
+        }
+      });
+
+      // Remove old rows
+      [].forEach.call(rows, function(row) {
+        tableBody.removeChild(row);
+      });
+
+      // Reverse the direction
+      directions[index] = direction === "asc" ? "desc" : "asc";
+
+      // Append new row
+      newRows.forEach(function(newRow) {
+        tableBody.appendChild(newRow);
+      });
+    };
+
+    [].forEach.call(headers, function(header, index) {
+      header.addEventListener("click", function() {
+        sortColumn(index);
+      });
+    });
+  }
+};
+
+const attachSearchClear = function() {
+  // Hook into the rendered button
+  document
+    .getElementById("search-clear-button")
+    .addEventListener("click", function() {
+      document.getElementById("search-input").value = "";
+      document.getElementById("search-form").submit();
+    });
+};
---
+++ src/Apps/StatCan.OrchardCore.Radar/Assets/js/vue-components/components/GlobalSearch.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="global-search-container">
+    <v-icon color="white" class="mr-5">
+      mdi-magnify
+    </v-icon>
+    <input
+      v-model.trim="searchText"
+      @input="isTyping = true"
+      class="global-search"
+      type="text"
+      :placeholder="placeholder"
+    />
+    <div class="dropdown-list" ref="dropdownResult" v-show="showDropdown">
+      <div v-show="projects.length > 0">
+        <div class="result-caption">
+          <v-icon color="#009688" class="mr-3">mdi-flask-outline</v-icon>
+          <div>{{ projectTitle }}</div>
+        </div>
+        <div
+          v-for="item in projects"
+          :key="item.name"
+          class="dropdown-item"
+        >
+          <a class="black--text" :href="item.AutoroutePart.Path">{{ item.DisplayText }}</a>
+        </div>
+      </div>
+
+      <div v-show="communities.length > 0">
+        <div class="result-caption">
+          <v-icon color="#009688" class="mr-3">mdi-account-multiple</v-icon>
+          <div>{{ communityTitle }}</div>
+        </div>
+        <div
+          v-for="item in communities"
+          :key="item.name"
+          class="dropdown-item"
+        >
+          <a class="black--text" :href="item.AutoroutePart.Path">{{ item.DisplayText }}</a>
+        </div>
+      </div>
+
+      <div v-show="events.length > 0">
+        <div class="result-caption">
+          <v-icon color="#009688" class="mr-3">mdi-calendar</v-icon>
+          <div>{{ eventTitle }}</div>
+        </div>
+        <div v-for="item in events" :key="item.name" class="dropdown-item">
+          <a class="black--text" :href="item.AutoroutePart.Path">{{ item.DisplayText }}</a>
+        </div>
+      </div>
+
+      <div v-show="proposals.length > 0">
+        <div class="result-caption">
+          <v-icon color="#009688" class="mr-3">mdi-alert-circle-outline</v-icon>
+          <div>{{ communityTitle }}</div>
+        </div>
+        <div
+          v-for="item in proposals"
+          :key="item.name"
+          class="dropdown-item"
+        >
+          <a class="black--text" :href="item.AutoroutePart.Path">{{ item.DisplayText }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from "lodash";
+
+export default {
+  props: [
+    "apiUrl",
+    "placeholder",
+    "projectTitle",
+    "eventTitle",
+    "communityTitle",
+    "proposalTitle"
+  ],
+  data: function() {
+    return {
+      searchText: "",
+      isTyping: false,
+      projects: [],
+      events: [],
+      proposals: [],
+      communities: [],
+      showDropdown: false
+    };
+  },
+  watch: {
+    searchText: _.debounce(function() {
+      this.isTyping = false;
+    }, 1000),
+    isTyping: function(value) {
+      if (!value) {
+        this.getResult(this.searchText);
+      }
+    }
+  },
+  created() {
+    document.addEventListener("click", this.documentClick);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.documentClick);
+  },
+  methods: {
+    getResult(searchText) {
+      fetch(this.apiUrl + `?searchText=${searchText}`, {
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.length > 0) {
+            this.showDropdown = true;
+          } else {
+            this.showDropdown = false;
+          }
+
+          this.communities = data.filter(
+            content => content.ContentType === "Community"
+          );
+          this.projects = data.filter(
+            content => content.ContentType === "Project"
+          );
+          this.events = data.filter(content => content.ContentType === "Event");
+          this.proposals = data.filter(
+            content => content.ContentType === "Proposal"
+          );
+        });
+    },
+    documentClick(e) {
+      let el = this.$refs.dropdownResult;
+      let target = e.target;
+      if (el !== target && !el.contains(target)) {
+        this.showDropdown = false;
+      }
+    },
+    isListEmpty(list) {
+      return list.length === 0;
+    }
+  }
+};
+</script>
