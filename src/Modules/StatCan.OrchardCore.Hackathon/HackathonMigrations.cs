b6f877e0688e0110dd51d0644b77f25631222b89
R Axalan <rubylynne.axalan@gmail.com>
2022-02-09 14:27:04 -0500

    WIP Adds Hired and HiredBy fields

--- src/Modules/StatCan.OrchardCore.Hackathon/HackathonMigrations.cs
+++ src/Modules/StatCan.OrchardCore.Hackathon/HackathonMigrations.cs
@@ -106,7 +106,8 @@ namespace StatCan.OrchardCore.Hackathon
             );
         }
 
-        private void CreateSkillsInterestForm() {
+        private void CreateSkillsInterestForm() 
+        {
             _contentDefinitionManager.AlterTypeDefinition("SkillsInterest", type => type
                 .DisplayedAs("SkillsInterest")
                 .Listable()
@@ -125,210 +126,226 @@ namespace StatCan.OrchardCore.Hackathon
                 .WithField("DegreesCompleted", field => field
                     .OfType("TextField")
                     .WithDisplayName("DegreesCompleted")
-                    .WithPosition("2")
+                    .WithPosition("4")
                 )
                 .WithField("PhoneNumber", field => field
                     .OfType("TextField")
                     .WithDisplayName("PhoneNumber")
-                    .WithPosition("3")
+                    .WithPosition("5")
                 )
                 .WithField("GcJobInterest", field => field
                     .OfType("TextField")
                     .WithDisplayName("GcJobInterest")
-                    .WithPosition("4")
+                    .WithPosition("6")
                 )
                 .WithField("JobType", field => field
                     .OfType("TextField")
                     .WithDisplayName("JobType")
-                    .WithPosition("5")
+                    .WithPosition("7")
                 )
                 .WithField("Coop", field => field
                     .OfType("TextField")
                     .WithDisplayName("Coop")
-                    .WithPosition("6")
+                    .WithPosition("8")
                 )
                 .WithField("Fswep", field => field
                     .OfType("TextField")
                     .WithDisplayName("Fswep")
-                    .WithPosition("7")
+                    .WithPosition("9")
                 )
                 .WithField("Location", field => field
                     .OfType("TextField")
                     .WithDisplayName("Location")
-                    .WithPosition("8")
+                    .WithPosition("10")
                 )
                 .WithField("Citizenship", field => field
                     .OfType("TextField")
                     .WithDisplayName("Citizenship")
-                    .WithPosition("9")
+                    .WithPosition("11")
                 )
                 .WithField("CitizenshipStatus", field => field
                     .OfType("TextField")
                     .WithDisplayName("CitizenshipStatus")
-                    .WithPosition("10")
+                    .WithPosition("12")
                 )
                 .WithField("FieldsOfInterest", field => field
                     .OfType("TextField")
                     .WithDisplayName("FieldsOfInterest")
-                    .WithPosition("11")
+                    .WithPosition("13")
                 )
                 .WithField("OtherFieldsOfInterest", field => field
                     .OfType("TextField")
                     .WithDisplayName("OtherFieldsOfInterest")
-                    .WithPosition("12")
+                    .WithPosition("14")
                 )
                 .WithField("OtherInterest", field => field
                     .OfType("TextField")
                     .WithDisplayName("OtherInterest")
-                    .WithPosition("13")
+                    .WithPosition("15")
                 )
                 .WithField("FieldsOfExpertise", field => field
                     .OfType("TextField")
                     .WithDisplayName("FieldsOfExpertise")
-                    .WithPosition("14")
+                    .WithPosition("16")
                 )
                 .WithField("OtherFieldsOfExpertise", field => field
                     .OfType("TextField")
                     .WithDisplayName("OtherFieldsOfExpertise")
-                    .WithPosition("15")
+                    .WithPosition("17")
                 )
                 .WithField("OtherExpertise", field => field
                     .OfType("TextField")
                     .WithDisplayName("OtherExpertise")
-                    .WithPosition("16")
+                    .WithPosition("18")
                 )
                 .WithField("Sql", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Sql")
-                    .WithPosition("17")
+                    .WithPosition("19")
                 )
                 .WithField("Sas", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Sas")
-                    .WithPosition("18")
+                    .WithPosition("20")
                 )
                 .WithField("R", field => field
                     .OfType("NumericField")
                     .WithDisplayName("R")
-                    .WithPosition("19")
+                    .WithPosition("21")
                 )
                 .WithField("Python", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Python")
-                    .WithPosition("20")
+                    .WithPosition("22")
                 )
                 .WithField("Java", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Java")
-                    .WithPosition("21")
+                    .WithPosition("23")
                 )
                 .WithField("Perl", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Perl")
-                    .WithPosition("22")
+                    .WithPosition("24")
                 )
                 .WithField("Scala", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Scala")
-                    .WithPosition("23")
+                    .WithPosition("25")
                 )
                 .WithField("Julia", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Julia")
-                    .WithPosition("24")
+                    .WithPosition("26")
                 )
                 .WithField("Matlab", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Matlab")
-                    .WithPosition("25")
+                    .WithPosition("27")
                 )
                 .WithField("Stata", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Stata")
-                    .WithPosition("26")
+                    .WithPosition("28")
                 )
                 .WithField("PowerBi", field => field
                     .OfType("NumericField")
                     .WithDisplayName("PowerBi")
-                    .WithPosition("27")
+                    .WithPosition("29")
                 )
                 .WithField("Qgis", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Qgis")
-                    .WithPosition("28")
+                    .WithPosition("30")
                 )
                 .WithField("Argis", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Arcgis")
-                    .WithPosition("29")
+                    .WithPosition("31")
                 )
                 .WithField("Grass", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Grass")
-                    .WithPosition("30")
+                    .WithPosition("32")
                 )
                 .WithField("CPlusPlus", field => field
                     .OfType("NumericField")
                     .WithDisplayName("CPlusPlus")
-                    .WithPosition("31")
+                    .WithPosition("33")
                 )
                 .WithField("CSharp", field => field
                     .OfType("NumericField")
                     .WithDisplayName("CSharp")
-                    .WithPosition("32")
+                    .WithPosition("34")
                 )
                 .WithField("Javascript", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Javascript")
-                    .WithPosition("33")
+                    .WithPosition("35")
                 )
                 .WithField("Html", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Html")
-                    .WithPosition("34")
+                    .WithPosition("36")
                 )
                 .WithField("Css", field => field
                     .OfType("NumericField")
                     .WithDisplayName("Css")
-                    .WithPosition("35")
+                    .WithPosition("37")
                 )
                 .WithField("OtherLanguage", field => field
                     .OfType("TextField")
                     .WithDisplayName("OtherLanguage")
-                    .WithPosition("36")
+                    .WithPosition("38")
                 )
                 .WithField("OtherLanguageRating", field => field
                     .OfType("NumericField")
                     .WithDisplayName("OtherLanguageRating")
-                    .WithPosition("37")
+                    .WithPosition("39")
                 )
                 .WithField("PublicProfilesLink", field => field
                     .OfType("TextField")
                     .WithDisplayName("PublicProfilesLink")
-                    .WithPosition("38")
+                    .WithPosition("40")
                 )
                 .WithField("GitHubLink", field => field
                     .OfType("TextField")
                     .WithDisplayName("GitHubLink")
-                    .WithPosition("39")
+                    .WithPosition("41")
                 )
                 .WithField("Experience", field => field
                     .OfType("TextField")
                     .WithDisplayName("Experience")
-                    .WithPosition("40")
+                    .WithPosition("42")
                 )
                 .WithField("Cv", field => field
                     .OfType("TextField")
                     .WithDisplayName("Cv")
                     .WithEditor("TextArea")
-                    .WithPosition("41")
+                    .WithPosition("43")
                 )
                 .WithField("Language", field => field
                     .OfType("TextField")
                     .WithDisplayName("Language")
+                    .WithPosition("3")
+                )
+                .WithField("Hired", field => field
+                    .OfType("BooleanField")
+                    .WithDisplayName("Hired")
                     .WithPosition("1")
                 )
+                .WithField("HiredBy", field => field
+                    .OfType("UserPickerField")
+                    .WithDisplayName("HiredBy")
+                    .WithPosition("2")
+                    .WithSettings(new UserPickerFieldSettings
+                    {
+                        DisplayedRoles = new[] { "Volunteer" },
+                    })
+                )
             );
+
         }
+
     }
 }
